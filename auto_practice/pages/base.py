e92021dc35803f3a2478440122381c54d128a027
Ivan Nenashev <inenashev@icloud.com>
2022-11-15 00:01:39 +0400

    first working proto

--- auto_practice/pages/base.py
+++ auto_practice/pages/base.py
@@ -2,7 +2,6 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver import Keys
 
 
 class BasePage:
